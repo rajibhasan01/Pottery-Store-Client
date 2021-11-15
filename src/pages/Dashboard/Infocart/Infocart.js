0fa06212bd35a9929921f71824a2ec589fd52175
unknown <hasan.rajib1996@gmail.com>
2021-11-15 20:57:38 +0600

    updated

--- src/pages/Dashboard/Infocart/Infocart.js
+++ src/pages/Dashboard/Infocart/Infocart.js
@@ -8,13 +8,13 @@ const Infocart = () => {
             <div className="featuredItem">
                 <span className="fs-5 robotoFont text-muted pb-2 d-block">Revanue</span>
                 <div className="featuredMoneyContainer">
-                    <span className="fs-4 robotoFont">$2,415</span>
-                    <span className="">
+                    <span className="fs-4 fontMoney robotoFont">$2,415</span>
+                    <span className="fontSizeValue">
                         -11.4
-                        <MdIcons.MdArrowDownward className="text-danger" />
+                        <MdIcons.MdArrowDownward className="text-danger fs-6" />
                     </span>
                 </div>
-                <span className="text-muted">Compared to last month</span>
+                <span className="spanFontSize">Compared to last month</span>
             </div>
         </div>
     );
