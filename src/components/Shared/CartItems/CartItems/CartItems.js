5a52e85ab587418c63f59498b4e9efdc5f40cbfe
unknown <hasan.rajib1996@gmail.com>
2021-11-27 14:03:23 +0600

    promo code set locally

--- src/components/Shared/CartItems/CartItems/CartItems.js
+++ src/components/Shared/CartItems/CartItems/CartItems.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from 'react';
 import useAuth from '../../../../hooks/useAuth';
 import useData from '../../../../hooks/useData';
 import CartItem from '../CartItem/CartItem';
+import './CartItems.css';
 
 const CartItems = ({ setCheckForPlaceOrder }) => {
     const { user } = useAuth();
     const [count, setCount] = useState(0);
     const [products, setProducts] = useState([]);
-    const [totalAmount, setTotalAmount] = useState([]);
+    const [totalAmount, setTotalAmount] = useState(0);
+    const [subTotal, setSubTotal] = useState(0);
+    const [voucher, setVoucher] = useState(0);
+    const [promo, setPromo] = useState(false);
+    const [promoCode, setPromoCode] = useState('');
     const [items] = useData();
+    const voucherCode = 'rajib';
 
     useEffect(() => {
         const items = JSON.parse(localStorage.getItem(`${user.email}_cart`));
@@ -43,9 +49,32 @@ const CartItems = ({ setCheckForPlaceOrder }) => {
 
     }, [products, items, user.email]);
 
+    // handle promo code
+    const handlePromoCode = () => {
+        setPromo(true);
+    };
 
 
+    // handlePromoCodeName
+    const handlePromoCodeName = e => {
+        const value = e.target.value
+        setPromoCode(value);
+    };
 
+    // set voucher percentage
+    const handlePromoCodeAction = () => {
+        if (voucherCode === promoCode) {
+            setVoucher(10);
+        }
+    };
+
+    // calculate total for each changes
+    useEffect(() => {
+        if (voucher) {
+            const newTotal = totalAmount * (100 - voucher) / 100;
+            setSubTotal(newTotal);
+        }
+    }, [totalAmount, voucher])
 
     // handle delete item from cart
     const handleDltItem = (id, size) => {
@@ -75,49 +104,77 @@ const CartItems = ({ setCheckForPlaceOrder }) => {
         }
     };
 
+    console.log('voucher', voucher);
+
 
     return (
-        <div className="bg-light p-2">
-            <h4 className="robotoFont fw-light text-muted pt-3">Total Products = {products?.length}</h4>
-            <hr />
-            <div className="table-responsive-sm">
-                <table className="widgetLgTable mx-auto">
-                    <thead>
-                        <tr className="">
-                            <th className="widgetLgTh robotoFont ps-md-5">Product</th>
-                            <th className="widgetLgTh robotoFont">Details</th>
-                            <th className="widgetLgTh robotoFont">Discount</th>
-                            <th className="widgetLgTh robotoFont">Amount</th>
-                            <th className="widgetLgTh robotoFont ps-1">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+        <>
+            <div className="bg-light p-2">
+                <h4 className="robotoFont fw-light text-muted pt-3">Total Products = {products?.length}</h4>
+                <hr />
+                <div className="table-responsive-sm">
+                    <table className="widgetLgTable mx-auto">
+                        <thead>
+                            <tr className="">
+                                <th className="widgetLgTh robotoFont ps-md-5">Product</th>
+                                <th className="widgetLgTh robotoFont">Details</th>
+                                <th className="widgetLgTh robotoFont">Discount</th>
+                                <th className="widgetLgTh robotoFont">Amount</th>
+                                <th className="widgetLgTh robotoFont ps-1">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                        {
-                            products?.map((product, key) => <CartItem
-                                key={key}
-                                product={product}
-                                handleDltItem={handleDltItem}
-                                handlePlus={handlePlus}
-                                handleMinus={handleMinus}
-                            />)
-                        }
+                            {
+                                products?.map((product, key) => <CartItem
+                                    key={key}
+                                    product={product}
+                                    handleDltItem={handleDltItem}
+                                    handlePlus={handlePlus}
+                                    handleMinus={handleMinus}
+                                />)
+                            }
 
-                        <tr className="border-bottom"><td className="displayHidden"> .</td></tr>
+                            <tr className="border-bottom"><td className="displayHidden"> .</td></tr>
 
-                        <tr className="text-start robotoFont fs-6 ">
-                            <td></td>
-                            <td></td>
-                            <td className="text-start robotoFont fs-6 ">Total =</td>
-                            <td className="text-start robotoFont text-danger">${totalAmount}</td>
+                            <tr className="text-start robotoFont fs-6 ">
+                                <td></td>
+                                <td></td>
+                                <td className="text-start robotoFont fs-6 ">Total =</td>
+                                <td className="text-start robotoFont text-danger">${totalAmount}</td>
 
-                        </tr>
-                    </tbody>
+                            </tr>
 
-                </table>
+                        </tbody>
+
+                    </table>
+                </div>
 
             </div>
-        </div>
+            {
+                !promo ?
+                    <small className="pt-3 d-block mx-auto">have a promo code! <span className="text-primary ms-0 cursor-pointer" onClick={handlePromoCode}>click here</span></small>
+                    :
+                    !subTotal ?
+                        <div className="pt-3">
+                            <input
+                                type="text"
+                                placeholder="promo code"
+                                className="inputBox ps-2"
+                                onBlur={handlePromoCodeName}
+                            />
+
+                            <button
+                                className="promoBtn btn"
+                                onClick={handlePromoCodeAction}
+                            >Apply</button>
+                        </div>
+                        :
+                        <div className="pt-3">
+                            <h5 className="text-muted robotoFont">Sub Total:<span className="text-danger">{subTotal}/=</span></h5>
+                        </div>
+            }
+        </>
     );
 };
 
